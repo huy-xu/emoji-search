a9a87fb417c830e8f3a793c314ddc728467df9fb
LoiIn <canducloi99@gmail.com>
2021-04-13 10:14:12 +0000

    fix some bug

--- src/hooks/EmojiResultRow.js
+++ src/hooks/EmojiResultRow.js
@@ -2,23 +2,22 @@ import React, {useState, useEffect} from 'react'
 import PropTypes from 'prop-types'
 import "./EmojiResultRow.css"
 
-function EmojiResultRow (){
-    const [emoji,setEmoji] = useState({title:'', symbol: ''})
-    const [codePointHex, setCodePointHex] = useState([emoji.symbol.codePointAt(0).toString(16)])
-    const [src, setSrc] = useState([`//cdn.jsdelivr.net/emojione/assets/png/${codePointHex}.png`])
+function EmojiResultRow (emojiData){
+    const codePointHex = emojiData.symbol.codePointAt(0).toString(16)
+    const src = `//cdn.jsdelivr.net/emojione/assets/png/${codePointHex}.png`
     
     return (
         <div  className="component-emoji-result-row copy-to-clipboard"
-        data-clipboard-text={emoji.symbol}>
-            <img alt={emoji.title} src={src} />
-            <span className="title">{emoji.title}</span>
+        data-clipboard-text={emojiData.symbol}>
+            <img alt={emojiData.title} src={src} />
+            <span className="title">{emojiData.title}</span>
             <span className="info">Click to copy emoji</span>
         </div>
     )
 }
 
 EmojiResultRow.propTypes = {
-    emoji : PropTypes.shape({
+    emojiData : PropTypes.shape({
         title: PropTypes.string,
         symbol: PropTypes.string
     })
